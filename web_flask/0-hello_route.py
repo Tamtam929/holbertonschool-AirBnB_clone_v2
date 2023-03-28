87a67c4eaa62b601a66e5efd217c8de12bde6792
CaseyCJC <knightlaggia@gmail.com>
2023-03-28 06:37:11 +0000

    task0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,5 +10,5 @@ def hello():
     return "Hello HBNB!"
 
 
-    if __name__ == "__main__":
-        app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
