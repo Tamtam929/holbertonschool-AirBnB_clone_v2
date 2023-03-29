af933486519dfd79a9cef05bd85e58f0430ef822
Tammy <5554@holbertonstudents.com>
2023-03-29 14:55:09 +0000

    1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-
+"""Script that starts a flask"""
 from flask import Flask
+
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def index():
-    return 'Hello HBNB!'
+    return("Hello HBNB!")
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return 'HBNB'
+    return("HBNB")
 
 
 if __name__ == '__main__':
