7c08627864944b6c0cf475f01517238ad7d61c8d
Tammy <5554@holbertonstudents.com>
2023-03-29 01:46:41 +0000

    task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,23 +1,19 @@
-<<<<<<< HEAD
-1
-=======
 #!/usr/bin/python3
-"""script that starts a flask application"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
-    return "Hello HBNB!"
+def index():
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return "HBNB"
+    return 'HBNB'
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000')
->>>>>>> 20ee44c886a05ea7e03b8a77b6571e8c92eb71b8
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
